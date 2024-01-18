1c76bc8125c0d4b8deb302bf6e6d589fce0838a7
Dillon-Todd <dillontodd10@gmail.com>
2024-01-18 17:46:53 -0600

    add layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import "./App.css";
+
 const App = () => {
-  return <div>App</div>;
+  const btnList = [
+    ["AC", "±", "%", "÷"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="],
+  ];
+
+  return (
+    <div className="container">
+      <div className="calculator-grid">
+        <div className="screen">
+          <div className="previous-operation">123 *</div>
+          <div className="current-operation">17</div>
+        </div>
+        {btnList.flat().map((btn, index) => (
+          <button key={index} className={btn === "0" ? "span-2" : ""}>
+            {btn}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
